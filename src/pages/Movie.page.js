2b1289186b5dea7598d94c2d59c953547cc2821e
JatinMaurya <132572196+JatinMaurya@users.noreply.github.com>
2023-05-23 16:01:28 +0530

    Add files via upload

---
+++ src/pages/Movie.page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Movie = () => {
+  return (
+    <>
+    <div>
+   <img src="https://assets-in.bmscdn.com/promotions/cms/creatives/1683178931546_dastaaneghazalbhopal.jpg" />
+   </div>
+    </>
+  );
+};
+
+export default Movie;
